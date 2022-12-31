52896fcc17612dfa4ff0867f39c0d31a38b37970
todaylab <jinrishouji@sina.com>
2022-12-31 22:30:09 +0800

    add full_game import

--- x/checkers/types/full_game.go
+++ x/checkers/types/full_game.go
@@ -1,6 +1,22 @@
+package types
+
+import (
+	"errors"
+	"fmt"
+
+	rules "github.com/alice/checkers/x/checkers/rules"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
 func (storedGame StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
 	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
 	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+}
+
+func (storedGame StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
+	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
+	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
 }
 
 func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
