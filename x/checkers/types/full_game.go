6a4bc18c1b0ad5ba2500ea9adb48f00fb3f485ae
todaylab <jinrishouji@sina.com>
2022-12-31 22:19:46 +0800

    Create full_game.go

---
+++ x/checkers/types/full_game.go
@@ -0,0 +1,29 @@
+func (storedGame StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
+	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
+	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+}
+
+func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
+	board, errBoard := rules.Parse(storedGame.Board)
+	if errBoard != nil {
+			return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
+	}
+	board.Turn = rules.StringPieces[storedGame.Turn].Player
+	if board.Turn.Color == "" {
+			return nil, sdkerrors.Wrapf(errors.New(fmt.Sprintf("Turn: %s", storedGame.Turn)), ErrGameNotParseable.Error())
+	}
+	return board, nil
+}
+
+func (storedGame StoredGame) Validate() (err error) {
+	_, err = storedGame.GetBlackAddress()
+	if err != nil {
+			return err
+	}
+	_, err = storedGame.GetRedAddress()
+	if err != nil {
+			return err
+	}
+	_, err = storedGame.ParseGame()
+	return err
+}
